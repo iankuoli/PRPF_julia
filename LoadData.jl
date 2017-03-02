e4b266a1ea70b4a664484c017de72a4af4080b91
IanKuo <iankuoli@gmail.com>
2017-03-02 17:00:53 +0800

    LoadUtilities has passed the unit test.

--- LoadData.jl
+++ LoadData.jl
@@ -18,12 +18,20 @@ function LoadUtilities(train_path::AbstractString, test_path::AbstractString, va
   #
   # Compute: # of user, # of items
   #
-  M = max(max(matIndex_train[:,1]), max(matIndex_test[:,1]), max(matIndex_valid[:,1]));
-  N = max(max(matIndex_train[:,2]), max(matIndex_test[:,2]), max(matIndex_valid[:,2]));
+  M = max(maximum(matIndex_train[:,1]), maximum(matIndex_test[:,1]), maximum(matIndex_valid[:,1]));
+  N = max(maximum(matIndex_train[:,2]), maximum(matIndex_test[:,2]), maximum(matIndex_valid[:,2]));
 
-  matX_train = sparse(matIndex_train[:,1], matIndex_train[:,2], matIndex_train[:,3], M, N);
-  matX_test = sparse(matIndex_test[:,1], matIndex_test[:,2], matIndex_test[:,3], M, N);
-  matX_valid = sparse(matIndex_valid[:,1], matIndex_valid[:,2], matIndex_valid[:,3], M, N);
+  matX_train = sparse(convert(Array{Int64,1}, matIndex_train[:,1]), convert(Array{Int64,1}, matIndex_train[:,2]), matIndex_train[:,3], M, N);
+  matX_test = sparse(convert(Array{Int64,1}, matIndex_test[:,1]), convert(Array{Int64,1}, matIndex_test[:,2]), matIndex_test[:,3], M, N);
+  matX_valid = sparse(convert(Array{Int64,1}, matIndex_valid[:,1]), convert(Array{Int64,1}, matIndex_valid[:,2]), matIndex_valid[:,3], M, N);
 
-  return matX_train, matX_test, matX_valid
+  return matX_train, matX_test, matX_valid, convert(Int64, M), convert(Int64, N)
 end
+
+#
+# // ----- Unit Test ----- //
+#
+train_path = "/home/ian/Dataset/LastFm_train.csv";
+test_path = "/home/ian/Dataset/LastFm_test.csv";
+valid_path = "/home/ian/Dataset/LastFm_valid.csv";
+matX_train, matX_test, matX_valid, M, N = LoadUtilities(train_path, test_path, valid_path)
